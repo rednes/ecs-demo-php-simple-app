ba0c3114bf8767171623cb7263d9a131add42b1e
rednes <528249+rednes@users.noreply.github.com>
2018-10-29 13:34:31 +0900

    test

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Green->Blue</p>
+                <p>Blue->Green OK</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
