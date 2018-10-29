90c93bc639084db20a805055915cb0917530b48e
rednes <528249+rednes@users.noreply.github.com>
2018-10-29 12:07:32 +0900

    test

--- src/index.php
+++ src/index.php
@@ -17,6 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
+                <p>Blue->Green</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
@@ -24,9 +25,6 @@
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                   ?>
-
-                <p>second deploy</p>
-
             </div>
         </div>
 
